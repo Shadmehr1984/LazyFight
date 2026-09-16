a0003593a57784caaa65a0a57db7173a4654fbff
shadmehr <ssszzzast@gmail.com>
2026-09-17 01:46:43 +0430

    Refactor

--- src/things/Inventory.py
+++ src/things/Inventory.py
@@ -2,34 +2,81 @@ from src.things.Item import Item
 
 class Inventory:
     def __init__(self) -> None:
-        self.items: list[Item] = []
+        self.items: dict[str, Item] = {}
     
-    def add_item(self, item):
-        for item_index in range(0, len(self.items)):
-            if (item == self.items[item_index]):
-                self.items[item_index].count += item.count
-                return
-        self.items.append(item)
+    def add_item(self, item: Item):
+        self.items[item.name] = item
     
-    def pick_item(self, item_name, item_count):
-        for item_index in range(0, len(self.items)):
-            if (item_name == self.items[item_index].name):
-                if (item_count > self.items[item_index].count):
-                    raise ValueError(f'not enough {item_name}, existing count:{self.items[item_index].count}')
-                else:
-                    self.items[item_index].count -= item_count
-                    if (self.items[item_index].count == 0):
-                        self.items.pop(item_index)
-                return True
-        print('you don\'t have this item')
-        return False
+    def exist_item(self, item_name: str):
+        item = self.items.get(item)
+        return item is not None
+
+    def exist_items(self, items_name: list[str]):
+        result = True
+        for item_name in items_name:
+            result = result and self.exist_item(item_name)
+        return result
     
-    def add_all_inventory(self, inventory):
-        for item in inventory.items:
+    def enough_item(self, item_name: str, item_count: int):
+        item = self.items[item_name]
+        
+        if (item is None):
+            raise IndexError('item not exists')
+        
+        return item.count >= item_count
+    def enough_items(self, items: dict[str, int]):
+        result = True
+        for item_name, item_count in items:
+            result = result and self.enough_item(item_name, item_count)
+        return result
+    
+    def pick_item(self, item_name: str, item_count: int):
+        item = self.items[item_name]
+        
+        if (item is None):
+            raise IndexError('item not exists')
+        
+        if (item.count < item_count):
+            raise ValueError('not enough item')
+        
+        item.count -= item_count
+        
+        if (item.count == 0):
+            del self.items[item_name]
+        
+        return True
+    
+    def pick_items(self, items: dict[str, int]):
+        for item_name, item_count in items:
+            if (not self.exist_item(item_name)):
+                raise IndexError(f'item {item_name} not exists')
+            if (not self.enough_item(item_name)):
+                raise ValueError(f'not enough {item_name}, count:{item_count}')
+        
+        for item_name, item_count in items:
+            item = self.items[item_name]
+            
+            item.count -= item_count
+            
+            if (item.count == 0):
+                del self.items[item_name]
+        
+        return True
+    
+    def get(self):
+        items : dict[str, int] = {}
+        
+        for item_name, item_count in self.items:
+            items[item_name] = item_count
+        
+        return items
+    
+    def add_all_inventory(self, inventory: 'Inventory'):
+        for item in inventory.items.values():
             self.add_item(item)
     
     def __str__(self) -> str:
         result = ''
-        for item in self.items:
+        for item in self.items.values():
             result += item.__str__() + "\n"
         return result[0:-1]
--- src/things/Item.py
+++ src/things/Item.py
@@ -1,7 +1,12 @@
 class Item:
     def __init__(self, name, count) -> None:
+        if (count < 0):
+            raise ValueError("count is a positive value")
         self.name = name
         self.count = count
+    
+    def get(self):
+        return {'name': self.name, 'count': self.count}
     
     def __eq__(self, __value: 'Item') -> bool:
         return self.name == __value.name
