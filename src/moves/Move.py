0a47e2bdea57bd468dce2e5e5836f921e5da7e8d
shadmehr <ssszzzast@gmail.com>
2026-09-21 05:42:57 +0330

    Feat: low values can increase

--- src/moves/Move.py
+++ src/moves/Move.py
@@ -2,12 +2,14 @@ from src.creatures.Creature import Creature
 from src.moves.MoveTarget import MoveTarget
 from src.things.Inventory import Inventory
 from random import randint
+from math import ceil
 
 class Move:
     move_target: MoveTarget = None
     
     __VALUE_RANK_UP_RATE = 20
-    __ACCURATE_RANK_UP_RATE = 50
+    __ACCURATE_RANK_UP_RATE = 35
+    __RANK_UP_REQUIRE_ITEM_COUNT_RATE = 2
     
     def __init__(self, rank, value, accurate, rank_up_require: dict[str, int]) -> None:
         self.rank = rank
@@ -26,8 +28,11 @@ class Move:
             return False
         
         self.rank += 1
-        self.value = int(self.value + (self.value/Move.__VALUE_RANK_UP_RATE))
-        self.accurate = int(self.accurate + (self.accurate/Move.__ACCURATE_RANK_UP_RATE))
+        self.value = ceil(self.value + (self.value/Move.__VALUE_RANK_UP_RATE))
+        if (self.accurate < 100): self.accurate = ceil(self.accurate + (self.accurate/Move.__ACCURATE_RANK_UP_RATE))
+        if (self.accurate > 100): self.accurate = 100
+        for item_name in self.rank_up_require:
+            self.rank_up_require[item_name] = ceil(self.rank_up_require[item_name] + (self.rank_up_require[item_name]/Move.__RANK_UP_REQUIRE_ITEM_COUNT_RATE))
     
     def move_take(self):
         self.is_take = randint(0, 100) <= self.accurate
